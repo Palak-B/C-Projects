28c859df028b7e7ae71a93f80572d48c8d33600c
Palak Bansal <palakb1406@gmail.com>
2017-02-26 01:21:00 +0530

    Add files via upload
    Linked Lists

--- main.cpp
+++ main.cpp
@@ -1,192 +1,177 @@
 #include <iostream>
 #include <stdlib.h>
+
 using namespace std;
-class sll
-{
-    sll* next;
-    int info;
 
+class list
+{
+    list* head=NULL;
+    list* tail=NULL;
+    list* next;
+    int val;
 public:
-    void insert_beg();
-    void insert_end();
-    void delete_beg();
-    void delete_end();
+    void ins();
     void display();
-    sll* header;
-    sll()
-    {
-        header=NULL;
-    }
+    void del();
+    void insb();
+    void reverse();
+    void delalt();
 };
-void sll::insert_beg()
+void list::ins()
 {
-    sll* temp=new sll;
-    temp->next=NULL;
-    cout<<"Enter value"<<endl;
-    cin>>temp->info;
-    if(header==NULL)
-    {
-      header=temp;
-      return ;
-    }
-    else
-    {
-      temp->next=header;
-      header=temp;
-      return;
-    }
+list* temp=new list;
+cout<<"Enter the information to be inserted"<<endl;
+int x;
+cin>>x;
+temp->val=x;
+temp->next=NULL;
+if(head==NULL)
+{
+    head=temp;
+    return;
 }
-void sll::insert_end()
+temp->next=head;
+head=temp;
+}
+void list::insb()
 {
-    sll* temp=new sll;
-    temp->next=NULL;
-    cout<<"Enter value"<<endl;
-    cin>>temp->info;
-    if(header==NULL)
-    {
-
-        //temp->next=cur;
-        header=temp;
-        //delete(cur);
-        return;
-    }
-    else
-    {
-    sll* cur=header;
-    while(cur->next!=NULL)
+    cout<<"Enter the element b4 which we have to insert the new element"<<endl;
+    int k;
+    cin>>k;
+    cout<<"Enter the element to insert"<<endl;
+    int c;
+    cin>>c;
+    list* cur=head;
+    while(cur->next->val!=k)
     {
         cur=cur->next;
     }
+    list* e =cur->next;
+    list* temp=new list;
+    temp->val=c;
+    temp->next=e;
     cur->next=temp;
-    return ;
-    }
+
 }
-void sll::delete_beg()
+void list::reverse()
 {
-    if(header==NULL)
-    {
-        cout<<"Underflow"<<endl;
-        return ;
-    }
-    sll* temp=header;
-    header=header->next;
-    delete(temp);
-    return;
-}
-void sll::delete_end()
-{
-    if(header==NULL)
-    {
-        cout<<"underflow";
-        return ;
-    }
-    sll* cur=header;
-    if(cur->next==NULL)
-    {
-        delete(cur);
-        header=NULL;
-        return;
-    }
-    while(cur->next->next!=NULL)
-    {
-        cur=cur->next;
-    }
-    delete(cur->next);
-    cur->next=NULL;
-    return;
-}
-void sll::display()
-{
-    sll* cur=header;
-    if(cur==NULL)
+    list*r=NULL;
+    list*p=head;
+    list*q=p->next;
+    if(head==NULL)
     {
         cout<<"empty";
         return;
     }
+    while(q!=NULL)
+    {
+        //r=p;
+        p->next=r;
+        r=p;
+        p=q;
+        q=q->next;
+    }
+    p->next=r;
+    head=p;
+
+}
+void list::delalt()
+{
+    list*cur=head;
+    if(head==NULL)
+    {
+        cout<<"empty";
+        return;
+    }
+    if(head->next==NULL)
+    {
+        return;
+    }
+    while(cur->next->next!=NULL)
+    {
+    list*c=cur->next;
+    cur->next=cur->next->next;
+    delete(c);
+    cur=cur->next;
+    }
+    if(cur->next)
+    {
+        delete(cur->next);
+        cur->next=NULL;
+    }
+}
+void list::del()
+{
+    if(head==NULL)
+    {
+        cout<<"empty";
+        return;
+    }
+list* cur=head;
+head=head->next;
+delete(cur);
+}
+void list::display()
+{
+    list*cur=head;
     while(cur!=NULL)
     {
-        cout<<cur->info;
+        cout<<cur->val<<" ";
         cur=cur->next;
     }
-    cout<<endl;
-    return;
 }
 int main()
 {
-    sll s;
-    //s.header=NULL;
-    //sll* h=s.header;
-cout<<"Press 1 for Stack and 2 for Queue"<<endl;
-int ch;
-cin>>ch;
-switch(ch)
-{
-case 1:
+    list l;
+    int k;
+    int p=1;
+    while(p)
     {
-
-        int k;
-        while(1)
-        {
-        cout<<"Implementing Stack"<<endl;
-        cout<<"1-push"<<endl<<"2-pop"<<endl<<"3-print"<<endl<<"4exit";
+        cout<<"1. insert 2. delete 3. display 4. exit 5. insert before 6. reverse 7. delete_alternate"<<endl;
         cin>>k;
         switch(k)
         {
-        case 1:
-            {
-                s.insert_end();
-                break;
-            }
-        case 2:
-            {
-                s.delete_end();
-                break;
-            }
-        case 3:
-            {
-                s.display();
-                break;
-            }
-        default:
-            exit(0);
-        }
-        }
+    case 1:
+        {
+        l.ins();
         break;
-    }
-case 2:
-    {
-
-        int k;
-        while(1)
-        {
-        cout<<"Implementing Queue"<<endl;
-        cout<<"1-push"<<endl<<"2-pop"<<endl<<"3-print"<<endl<<"4-exit";
-        cin>>k;
-        switch(k)
-        {
-        case 1:
-            {
-                s.insert_end();
-                break;
-            }
-        case 2:
-            {
-                s.delete_beg();
-                break;
-            }
-        case 3:
-            {
-                s.display();
-                break;
-            }
-        default:
-            exit(0);
         }
-    }
-    break;
-}
-    //cout << "Hello world!" << endl;
+    case 2:
+        {
+        l.del();
+        break;
+        }
+    case 3:
+        {
+        l.display();
+        break;
+        }
+    case 4:
+        {
+        exit(0);
+        break;
+        }
+    case 5:
+        {
+            l.insb();
+            break;
+        }
+    case 6:
+        {
+            l.reverse();
+            break;
+        }
+    case 7:
+        {
+            l.delalt();
+            break;
+        }
+    default:
+        cout<<"invalid";
 
-}
- return 0;
+    }
+    }
+
+    //cout << "Hello world!" << endl;
+    return 0;
 }
